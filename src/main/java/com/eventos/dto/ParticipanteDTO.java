4377da86cd55e837c85f14f3befba3ffef554b97
BrunaSS-18 <bestudante206@gmail.com>
2026-09-14 10:59:34 -0300

    feat: dto, service e repository do participante

---
+++ src/main/java/com/eventos/dto/ParticipanteDTO.java
@@ -0,0 +1,12 @@
+package com.eventos.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ParticipanteDTO(
+
+        @NotBlank(message = "O nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "O email é obrigatório")
+        String email
+) {}
