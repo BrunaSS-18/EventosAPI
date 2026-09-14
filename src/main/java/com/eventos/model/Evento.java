25f2a68d410f2e2ca19c741150080c87c620765b
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-09-14 09:24:10 -0300

    Feat: configurações iniciais

---
+++ src/main/java/com/eventos/model/Evento.java
@@ -0,0 +1,30 @@
+package com.eventos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private LocalDate data;
+
+    private String local;
+
+    @Column(nullable = true)
+    private Integer capacidadeMaxima;
+}
+
